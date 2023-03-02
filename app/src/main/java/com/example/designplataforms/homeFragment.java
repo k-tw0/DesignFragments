f2d62cfac5f08ce406f8f304801c4a798d6caed6
k-tw0 <francus_the_santos@gmail.com>
2023-03-02 03:19:36 -0300

    MediaPlayerSecond

--- app/src/main/java/com/example/designplataforms/homeFragment.java
+++ app/src/main/java/com/example/designplataforms/homeFragment.java
@@ -1,19 +1,44 @@
 package com.example.designplataforms;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class homeFragment extends Fragment {
 
+    Button btnPlayAudio;
+    MediaPlayer mp;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        // return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+        btnPlayAudio =  v.findViewById(R.id.btnPlayAudio);
+
+        btnPlayAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp = MediaPlayer.create(getActivity(), R.raw.sound);
+                mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.start();
+                    }
+                });
+            }
+        });
+
+        return v;
     }
 }
